28bf92581e2f1d1a2a772373d1ed1ac4856d41b3
Edd Sowden <e@e26.co.uk>
2011-11-09 11:56:55 +0000

    Make install.sh work to just install all files

--- install.sh
+++ install.sh
@@ -1,19 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
 # Inspired and in parts borrowed from 
 # https://raw.github.com/thoughtbot/dotfiles/master/install.sh
 
+black='\033[0m'
+red='\033[0;31m'
+green='\033[0;32m'
+
 for name in *; do
   target="$HOME/.$name"
-  if [ -e "$target" ]; then
-    if [ ! -L "$target" ]; then
-      echo 'rm "$target"'
-      echo 'ln -s "$PWD/$name" "$target"'
+  if [ "$name" != 'install.sh' ]; then
+    if [ -e "$target" ]; then
+      echo -e "${red}Deleting old $target${black}"
+      rm "$target"
     fi
-  else
-    if [ "$name" != 'install.sh' ]; then
-      echo "Creating $target"
-      echo 'ln -s "$PWD/$name" "$target"'
+    if [ -L "$target" ]; then
+      echo -e "${red}Deleting old $target${black}"
+      rm "$target"
     fi
+    echo -e "${green}Creating $target${black}"
+    ln -s "$PWD/$name" "$target"
   fi
 done
